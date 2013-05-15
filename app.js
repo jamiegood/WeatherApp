c729f575359ac5c27466ef2af98dde9540913a17
admin <jgood@paddypower.com>
2013-05-15 12:04:20 +0100

    Added About Tab Panel

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ Ext.application({
     ],
 
     views: [
-        'Main'
+        'Main',
+        'About'
     ],
 
     icon: {
@@ -55,17 +56,6 @@ Ext.application({
 
         // Initialize the main view
         Ext.Viewport.add(Ext.create('WeatherApp.view.Main'));
-    },
-
-    onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
-                }
-            }
-        );
     }
+
 });
