a8f223c25623db317968c72e99cf05a7963f486e
jamiegood <jamiegood@gmail.com>
2013-10-15 21:50:17 +0100

    Adding 5 day forecast API to config

--- app/util/Config.js
+++ app/util/Config.js
@@ -3,11 +3,22 @@ Ext.define('WeatherApp.util.Config', {
     alias : 'widget.appConfigUtil',
     config : {
         openweatherimages : 'http://openweathermap.org/img/w/',
-        weatherAPI: 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=',
+        base: 'http://api.openweathermap.org/data/2.5/', 
+        daily: 'weather?units=metric&q=',
+        fiveDay: 'forecast/?mode=json&units=metric&cnt=5&daily&q=',
     	defaultCountry: 'Dublin,ie',
     },
     constructor: function(config) {
         this.initConfig(config);
         this.callParent([config]);
+    },
+    daily: function() {
+
+    }, 
+    forecast: function() {
+        
     }
+
 })
+
+//http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=7
